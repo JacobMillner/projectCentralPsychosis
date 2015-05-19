a404d853eb63f91476ce5f1c3df5ca8ebd6015d1
Jacob Millner <jacob.millner@outlook.com>
2015-05-19 16:39:59 -0400

    Added spikes and checkpoints + ability to respawn.

---
+++ Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Checkpoint : MonoBehaviour {
+
+	public LevelManager levelManager;
+	
+	// Use this for initialization
+	void Start () {
+		
+		levelManager = FindObjectOfType<LevelManager> ();
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.name == "Player") 
+		{
+			levelManager.currentCheckpoint = gameObject;
+		}
+	}
+}
