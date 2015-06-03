f404452c96d280de8bb6c27812b5c80549233761
Jacob Millner <jacob.millner@outlook.com>
2015-06-02 21:55:48 -0400

    Added in particle systems for respawn and death.

---
+++ Assets/Scripts/DestroyFinishedParticle.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyFinishedParticle : MonoBehaviour {
+
+	private ParticleSystem thisParticleSystem;
+
+	// Use this for initialization
+	void Start () {
+		thisParticleSystem = GetComponent<ParticleSystem> ();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//probably fix this, this is dumb
+		if (thisParticleSystem.isPlaying) {
+			return;
+		}
+
+		Destroy (gameObject);
+	
+	}
+}
--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -7,6 +7,11 @@ public class LevelManager : MonoBehaviour {
 
 	private PlayerController player;
 
+	public GameObject deathParticle;
+	public GameObject respawnParticle;
+
+	public float respawnTimer;
+
 	// Use this for initialization
 	void Start () {
 
@@ -21,7 +26,19 @@ public class LevelManager : MonoBehaviour {
 
 	public void RespawnPlayer()
 	{
+		StartCoroutine ("RespawnPlayerCo");
+	}
+
+	public IEnumerator RespawnPlayerCo()
+	{
+		Instantiate (deathParticle, player.transform.position, player.transform.rotation);
+		player.enabled = false;
+		player.GetComponent<Renderer>().enabled = false;
 		Debug.Log ("Respawned");
+		yield return new WaitForSeconds (respawnTimer);
 		player.transform.position = currentCheckpoint.transform.position;
+		Instantiate (respawnParticle, currentCheckpoint.transform.position, currentCheckpoint.transform.rotation);
+		player.enabled = true;
+		player.GetComponent<Renderer>().enabled = true;
 	}
 }
